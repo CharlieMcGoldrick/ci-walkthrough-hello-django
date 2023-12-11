a75c3c5f3c4a8767daba02b7951a444966d56823
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-11 00:52:56 +0000

    Fix: secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,7 +23,7 @@ if os.path.isfile('env.py'):
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.SECRET_KEY
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
