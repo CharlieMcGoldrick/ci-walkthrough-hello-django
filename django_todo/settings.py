3bb1abc4b7a0133029499bbc68bc82b51f0b4113
CharlieMcGoldrick <charliemcgoldrick1995@gmail.com>
2023-12-11 00:49:52 +0000

    Fix: settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,15 +23,13 @@ if os.path.isfile('env.py'):
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = [env.SECRET_KEY]
+SECRET_KEY = env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = [env.DEBUG]
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 # Allowed hosts
 ALLOWED_HOSTS = ['ci-walkthrough-hello-django-c3c355d3169d.herokuapp.com']
-
-
 
 # Application definition
 
